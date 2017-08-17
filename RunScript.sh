7f0868edd47ee7ff6e3b5baaf0e8252b4736d6fa
reihaneh-torkzadehmahani <reihaneh.torkzadehmahani@gmail.com>
2017-08-16 21:55:49 -0700

    Update RunScript.sh

--- RunScript.sh
+++ RunScript.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 clear
 echo "Start Installing Dependencies ..."
-sudo apt-get update > /dev/null 2>&1
+sudo apt-get update 
 echo "Update ..."
-sudo apt-get install -y build-essential > /dev/null 2>&1
-echo "Build ..."
-sudo apt-get install -y python-pip > /dev/null 2>&1
-echo "Pip ..."
+sudo apt-get install -y build-essential 
+sudo apt-get install -y python-pip 
+
 
 echo "Installing Toil  ..."
 pip install toil
 
 echo "Installing java8  ..."
-sudo apt-get install software-properties-common> /dev/null 2>&1
-sudo add-apt-repository ppa:webupd8team/java -y > /dev/null 2>&1
-echo "Update ..."
-sudo apt-get update > /dev/null 2>&1
-echo "Installer ..."
+sudo apt-get install software-properties-common 
+sudo add-apt-repository ppa:webupd8team/java -y 
+sudo apt-get update 
 sudo apt-get install -y oracle-java8-installer
-echo "......."
-sudo apt-get install -y oracle-java8-set-default > /dev/null
+sudo apt-get install -y oracle-java8-set-default 
 
 echo "Installing gnuplot  ..."
-sudo apt-get install -y gnuplot > /dev/null 2>&1
-sudo apt-get update> /dev/null 2>&1
+sudo apt-get install -y gnuplot 
+sudo apt-get update
 
 echo "Installing canu  ..."
 sudo git clone https://github.com/marbl/canu.git
-sudo rm -r /home/ubuntu/canu/src/pipelines/canu/Defaults.pm
-sudo cp -r /home/ubuntu/canu/src/pipelines/canu/ReihanehScripts/Defaults.pm /home/ubuntu/canu/src/pipelines/canu
+sudo rm -r ./src/pipelines/canu/Defaults.pm
+sudo cp -r ./src/pipelines/canu/plScripts/Defaults.pm ./src/pipelines/canu
 cd canu/src
 make
-cd /home/ubuntu/canu/Linux-amd64/bin
-sudo cp -r /home/ubuntu/canu/src/pipelines/canu/ReihanehScripts/*.pl /home/ubuntu/canu/Linux-amd64/bin
-sudo cp -r /home/ubuntu/ForGit/ToilCanu.py /home/ubuntu/canu/Linux-amd64/bin
+cd ./Linux-amd64/bin
+sudo cp -r ./src/pipelines/canu/plScripts/*.pl ./Linux-amd64/bin
+sudo cp -r ./src/pipelines/canu/plScripts/ToilCanu.py ./Linux-amd64/bin
