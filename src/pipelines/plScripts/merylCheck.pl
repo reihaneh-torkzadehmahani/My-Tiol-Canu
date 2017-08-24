3afd41ecc2139ee3433f9a5e263902ae6e6733b6
reihaneh-torkzadehmahani <reihaneh.torkzadehmahani@gmail.com>
2017-08-24 16:01:01 -0700

    Add files via upload

---
+++ src/pipelines/plScripts/merylCheck.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use Cwd qw(getcwd abs_path);
+
+use lib "$FindBin::RealBin/lib";
+use lib "$FindBin::RealBin/lib/canu/lib/perl5";
+use lib "$FindBin::RealBin/lib/canu/lib64/perl5";
+
+use File::Path 2.08 qw(make_path remove_tree);
+
+use Carp;
+
+use canu::Defaults;
+use canu::Execution;
+
+use canu::Configure;
+
+use canu::Grid;
+use canu::Grid_Cloud;
+use canu::Grid_SGE;
+use canu::Grid_Slurm;
+use canu::Grid_PBSTorque;
+use canu::Grid_LSF;
+use canu::Grid_DNANexus;
+
+use canu::Gatekeeper;
+use canu::Meryl;
+use canu::OverlapInCore;
+use canu::OverlapMhap;
+use canu::OverlapMMap;
+use canu::OverlapStore;
+
+use canu::CorrectReads;
+use canu::ErrorEstimate;
+
+use canu::OverlapBasedTrimming;
+
+use canu::OverlapErrorAdjustment;
+use canu::Unitig;
+use canu::Consensus;
+use canu::Output;
+
+use canu::HTML;
+
+#  Initialize our defaults.  Must be done before defaults are reported in printOptions() below.
+
+
+#  The bin directory is needed for -version, can only be set after setDefaults(), but really should be
+#  set after checkParameters() so it can know pathMap.
+
+my $cmd     = undef;              #  Temporary string passed to system().
+my $asm     = undef;              #  Name of our assembly.
+my $mode=undef; 
+my $filename="/tmp/MC1Parameters.txt";
+
+$mode="run";
+$asm="ecoli";
+
+chdir('./ecoil-oxford2');
+
+$ENV{'CANU_DIRECTORY'} = getcwd();
+
+
+restoreParameters();
+
+#------------------------------------------------------------------------------------
+my $Step = $ARGV[0];
+
+if ($Step eq "correct"){       
+	foreach (1..getGlobal("canuIterationMax") + 1){
+		merylCheck1($asm, "cor");
+		merylCheck2($asm, "cor", "correction/0-mercounts")}
+
+}elsif ($Step eq "trim"){
+	foreach (1..getGlobal("canuIterationMax") + 1){
+                merylCheck1($asm, "obt");
+                merylCheck2($asm, "obt")}
+
+}elsif ($Step eq "assemble"){
+	foreach (1..getGlobal("canuIterationMax") + 1){
+                merylCheck1($asm, "utg");
+                merylCheck2($asm, "utg")}
+}else{
+die "Step is not valid!!! \n"; }
+
+#------------------------------------------------------------------------------------
+
+saveParameters();
+
+
